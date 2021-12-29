41994d0e3588567924026dec2e6f5c7cf8c3c782
Alain <93736888+developeralain@users.noreply.github.com>
2021-12-29 10:54:58 -0500

    Create inheritanceassignment2.0.py

---
+++ inheritanceassignment2.0.py
@@ -0,0 +1,19 @@
+class Player:
+    user_name = 'No name provided'
+    password = '1234234f'
+    email = ' '
+    server_type = 'Please choose PVP or PVE'
+    character_name = ' '
+    race = ' '
+    gender = ' '
+    health = '100'
+    
+
+class Mage(User):
+    spell_power = '100'
+    mana = '80'
+    
+
+class Warrior(User):
+    rage = '0'
+    weapon = 'sword of the bear'
