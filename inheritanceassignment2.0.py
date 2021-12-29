ab30bc7265db240b1456bb7cc5da8eed4b66a30c
Alain <93736888+developeralain@users.noreply.github.com>
2021-12-29 11:06:31 -0500

    Update inheritanceassignment2.0.py
    Added comments to my code and instantiated the mage and warrior classes, using a print function to demonstrate some unique attributes as well as those carried over from parent class

--- inheritanceassignment2.0.py
+++ inheritanceassignment2.0.py
@@ -1,5 +1,9 @@
+
+#this defines the Player parent class. It has attributes that all children
+#will possess. The 'Player' here is supposed to represent a video game player
+#for an RPG game that features classes such as mages and warriors.
 class Player:
-    user_name = 'No name provided'
+    user_name = 'JacobRules'
     password = '1234234f'
     email = ' '
     server_type = 'Please choose PVP or PVE'
@@ -8,12 +12,28 @@ class Player:
     gender = ' '
     health = '100'
     
-
+#this is the first child of parent Player. Only mages have spell power and mana.
+#it will posses all traits of the player parent as these apply.
 class Mage(Player):
     spell_power = '100'
     mana = '80'
     
-
+#this is the second child of a parent Player. Only warriors have rage as a
+#resource and only they use weapons. 
 class Warrior(Player):
     rage = '0'
     weapon = 'sword of the bear'
+
+#here you can designate a mage as 'Evandros' and instantiate him.
+Evandros = Mage
+#here you can see what his starting mana pool is
+print(Evandros.mana)
+#here you can see the username is carried over from parent class Player
+print(Evandros.user_name)
+
+#here you can designate a warrior as 'Savages'
+Savages = Warrior
+#here you can see what his starting weapon is
+print(Savages.weapon)
+#here you can see the username is carried over from parent class Player
+print(Savages.user_name)
