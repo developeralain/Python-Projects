a3bfdf3c8eee1fade317ea84a06b619036c0a719
Alain <93736888+developeralain@users.noreply.github.com>
2021-12-31 11:00:50 -0500

    Update encapsulationassignment.py

--- encapsulationassignment.py
+++ encapsulationassignment.py
@@ -1,23 +1,25 @@
+#chicken class is created
 class Chicken:
-    def __init__(self):
+    def __init__(self):#a function that allows initialization of a chicken object
         self._varPeck = "I have pecked the ground in search of seeds and worms"
         self.__varFly = "The chicken attempts to fly, but fails miserably"
 
-    def pecker(self):
+    def pecker(self):#function that prints _varPeck variable
         print(self._varPeck)
 
-    def flyer(self):
+    def flyer(self):#function that prints __varFly variable
         print(self.__varFly)
 
-    def changeFlight(self,flight):
+    def changeFlight(self,flight): #function that allows user to pass new values
+                                    #into __varFly variable
         self.__varFly = flight
         
 
-Flightmaster = Chicken()
-Flightmaster.pecker()
-Flightmaster.flyer()
-Flightmaster.self_varPeck = "I pecked your nose"
-Flightmaster.changeFlight("The chicken tries hard--real hard--and then fails.")
-Flightmaster.flyer()
+Flightmaster = Chicken()#Chicken class initialized as object stored in Flightmaster
+Flightmaster.pecker()#prints _varPeck
+Flightmaster.flyer()#prints __varFly
+Flightmaster.changeFlight("The chicken tries hard--real hard--and then fails.")#changes __varFly value
+Flightmaster.flyer()#prints new __varFly value
+
 
     
